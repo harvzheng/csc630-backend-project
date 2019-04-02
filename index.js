613ee44c4d5d8810ac43c71fe74ed158657536dc
lmaertens <44585549+lmaertens@users.noreply.github.com>
2019-04-02 10:31:31 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const https = require('https')
+const app = express()
+const port = 4000
+const onlineport = process.env.PORT
+const api_key = "AIzaSyBDKo6Hf-ZLyroGPc-3GUpQVjorWvVMI7g"
+
+let data = '';
+let street = "Bachstrasse"
+let number = 26
+let town = "Adelebsen"
+let state = "Niedersachsen"
+let country = "Germany"
+
+https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${number}+${street}+${town},${state}+${country}&key=${api_key}`, (res) => {
+ /*give credit for this*/
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+
+})
+
+app.get('/', (req, res) => res.send(JSON.parse(data)['results'][0]['geometry']['location']))
+app.listen(onlineport || port ,  () => console.log(`Example app listening on port ${port}!`));
+/* promises */
