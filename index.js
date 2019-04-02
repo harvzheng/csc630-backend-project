b721f22f0de63fcbba209959e3e37fa4392c03fc
lmaertens <44585549+lmaertens@users.noreply.github.com>
2019-04-02 12:34:07 -0400

    changed env variable

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const https = require('https')
 const app = express()
 const port = 4000
 const onlineport = process.env.PORT
-const api_key = process.env.API_KEY
+const api_key =  process.env.API_KEY
 
 let data = '';
-let street = "Bachstrasse"
-let number = 26
-let town = "Adelebsen"
-let state = "Niedersachsen"
-let country = "Germany"
+let street = "Main Street"
+let number = 180
+let town = "Andover"
+let state = "MA"
+let country = "US"
 
-https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${number}+${street}+${town},${state}+${country}&key=${api_key}`, (res) => {
- /*give credit for this*/
-
+https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${number}+${street}+${town},${state}+${country}&key=${api_key}`, (res) => { 
+/*give credit for this*/
     console.log(`API KEY is ${api_key}`)
     res.on('data', (chunk) => {
         data += chunk;
