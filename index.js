77ff3157b2097f20dbd951a035b9963892497a69
Harvey Zheng <zheng.harvey5@gmail.com>
2019-04-04 12:44:44 -0400

    fix index.js

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
-const express = require('express')
-const https = require('https')
-const app = express()
-const port = 4000
-const onlineport = process.env.PORT
-const api_key = "AIzaSyBDKo6Hf-ZLyroGPc-3GUpQVjorWvVMI7g"
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const http = require('http');
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
 
-let data = '';
-let street = "Main Street"
-let number = 180
-let town = "Andover"
-let state = "MA"
-let country = "US"
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'project1',
+});
 
-https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${number}+${street}+${town},${state}+${country}&key=${api_key}`, (res) => { 
-/*give credit for this */
-    res.on('data', (chunk) => {
-        data += chunk;
-    });
-})
-app.get('/', (req, res) => res.send(JSON.parse(data)['results'][0]['geometry']['location']))
-app.listen(onlineport || port ,  () => console.log(`Example app listening on port ${port}!`));
-/* promises */
+con.connect(function(err){
+  if (err) throw err
+  console.log("Connected with database");
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const server = app.listen(process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+  res.send("test");
+});
+
+//much of this code is modified from: https://medium.com/@avanthikameenakshi/building-restful-api-with-nodejs-and-mysql-in-10-min-ff740043d4be
+
+app.get('/users', function(req, res) {
+  con.query('SELECT * FROM users', function(error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+
+app.get('/users/:id', function(req, res) {
+  con.query('SELECT * FROM users WHERE id=?', [req.params.id], function(error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+
+app.post('/users', function (req, res) {
+  console.log(req.body)
+  con.query('INSERT INTO users (display_name, username, latitude, longitude) VALUES (?, ?, ?, ?)', [req.body.display_name, req.body.username, req.body.latitude, req.body.longitude], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+app.put('/users', function (req, res) {
+   con.query('UPDATE `users` SET `display_name`=?,`username`=?,`latitude`=?,`longitude`=? where `id`=?', [req.body.display_name, req.body.username, req.body.latitude, req.body.longitude, req.body.id], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+app.delete('/users', function (req, res) {
+   con.query('DELETE FROM `users` WHERE `id`=?', [req.body.id], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end('Record has been deleted!');
+	});
+});
