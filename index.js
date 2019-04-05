a892a8fca30e39065456a83c889d40cd7a0c6757
lmaertens <44585549+lmaertens@users.noreply.github.com>
2019-04-04 22:48:58 -0400

    Added comments

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/* 
+A CSC630 project by Harvey Zheng and Liv Martens. 
+The source code is available on GitHub and the actual server can be
+accessed at csc-630-project.herokuapp.com. The code can be run locally if a .env file with a valid API key is present.
+*/
+
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const http = require('http');
@@ -5,8 +11,9 @@ const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const https = require('https')
-
+const server = app.listen(process.env.PORT);
 const con = mysql.createConnection({
+//if this is used locally, a .env file is required
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -31,10 +38,9 @@ con.connect(function(err) {
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const server = app.listen(process.env.PORT);
 
 app.get('/', function(req, res) {
-  res.send("test");
+  res.send("CSC630 project by Harvey and Liv");
 });
 
 //much of this code is modified from: https://medium.com/@avanthikameenakshi/building-restful-api-with-nodejs-and-mysql-in-10-min-ff740043d4be
@@ -92,7 +98,7 @@ app.get('/poi', function (req, res) {
 app.post('/poi', function (req, res) {
   let data = ''
   https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.house_number}+${req.body.street}+${req.body.city},${req.body.state}+${req.body.country}&key=${process.env.API_KEY}`, (response) => {
-  /*give credit for this */
+  //http request handled in a way that is modified from https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html  
       response.on('data', (chunk) => {
           data += chunk;
       })
@@ -111,7 +117,7 @@ app.post('/poi', function (req, res) {
 app.put('/poi', function (req, res) {
   let data = ''
   https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.house_number}+${req.body.street}+${req.body.city},${req.body.state}+${req.body.country}&key=${process.env.API_KEY}`, (response) => {
-  /*give credit for this */
+  //http request handled in a way that is modified from https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
       response.on('data', (chunk) => {
           data += chunk;
       })
